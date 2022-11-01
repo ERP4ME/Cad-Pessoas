702c87f4748ff319dfa70ef348ef71c076bff76e
ERP4ME <erp4me.automatizado@gmail.com>
2022-11-01 11:40:33 -0300

    ajuste no logn, parou de funcionar via API

--- cypress/support/commands-login.js
+++ cypress/support/commands-login.js
@@ -1,12 +1,16 @@
 
+/*
 Cypress.Commands.add('login', (
   username = Cypress.env('USER_EMAIL'),
   password = Cypress.env('USER_PASSWORD')
 ) => {
   cy.session([username, password], () => {
+    onBeforeLoad: window.localStorage.setItem('ULTIMA_EXIBICAO_NOVIDADES', '14/10/2022')
+    onBeforeLoad: window.localStorage.setItem('ARRAY_CRMS_NAO_EXIBIR_NOVIDADES', '["355672"]')
     window.localStorage.setItem('CHAVE_NAOEXIBIR_TOAST_MANUTENCAO_SEFAZ', 'true')
     // onBeforeLoad: window.localStorage.setItem('KOOPON_NOVIDADES_VERSAO_2_21', 'true')
     window.localStorage.setItem('CHAVE_NAOEXIBIR_TOAST_VENCIMENTO_CERTIFICADO', 'true')
+   
     cy.request({
       method: 'POST',
       url: '/passaporte-rest-api/rest/login',
@@ -25,27 +29,65 @@ Cypress.Commands.add('login', (
   })
 })
 
+*/
+
+Cypress.Commands.add('login', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  cy.session([username, password], () => {
+    cy.intercept('GET', '/core/app/views/core_selecao_empresas_view.html').as("AguardarPagina")
+    cy.intercept('GET', '/koopon-core-rest-api/empresa/configuracoes/emissao/nota').as("AguardarEmpresa")
 
 
-Cypress.Commands.add('AcessarSistema', () => {
-  cy.visit('/')
-  cy.get(':nth-child(3) > .alt-lista-item-container')
-    .should('be.visible')
-    .click()
-  cy.intercept('GET', '/koopon-core-rest-api/empresa/configuracoes/emissao/nota').as('AguardarPagina')
-  cy.wait('@AguardarPagina').its('response.statusCode').should('be.equal', 200)
-  cy.get('.alt-titulo-view-container li b')
-    .should('be.visible')
 
+    onBeforeLoad: window.localStorage.setItem('CHAVE_NAOEXIBIR_TOAST_VENCIMENTO_CERTIFICADO', 'true')
+    onBeforeLoad: window.localStorage.setItem('ULTIMA_EXIBICAO_NOVIDADES', '14/10/2022')
+    onBeforeLoad: window.localStorage.setItem('ARRAY_CRMS_NAO_EXIBIR_NOVIDADES', '["401241"]')
+    cy.visit('/')
+    // cy.pause()
+    cy.get('#email-login')
+      .clear()
+      .type(username, { delay: 0 })
+    cy.get('.panel-body input[type="password"]').type(password, { delay: 0 }, { log: false })
+    cy.get('#login-passaporte').click()
+    cy.wait("@AguardarPagina").its("response.statusCode").should("be.equal", 200);
+    cy.get(":nth-child(3) > .alt-lista-item-container").click();
+    cy.AcessarSistema()
+
+  })
 })
 
 
+Cypress.Commands.add('AcessarSistema', () => {
+  cy.wait("@AguardarEmpresa").its("response.statusCode").should("be.equal", 200);
+  cy.get('.active > b', { timeout: 10000 })
+    .should('be.visible', { timeout: 10000 })
+
+})
+
+/*
+
+Cypress.Commands.add('AcessarSistema', () => {
+  cy.visit('/')
+  
+  cy.get(':nth-child(2) > .alt-lista-item-container')
+  .click()
+  cy.intercept('GET', '/passaporte-rest-api/rest/produtos').as("AguardarPagina")
+  cy.wait("@AguardarPagina").its("response.statusCode").should("be.equal", 200);
+  cy.contains('Painel Administrativo')
+  .should('be.visible')
+  
+
+})
+
+*/
 Cypress.Commands.add('AcessarPessoas', () => {
   cy.visit('/')
   cy.get('#koopon-cabecalho-navbar-cadastro')
     .should('be.visible')
     .click({ force: true })
-  cy.get('a[href="/pessoa/pessoas"]', { timeout: 30000 })
+    cy.get('#koopon-cabecalho-navbar-cadastro-pessoa-adicionar')
     .should('be.visible')
     .click({ force: true })
 
