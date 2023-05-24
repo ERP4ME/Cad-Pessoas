6e9ced112b19b2f510687be95a98279dfb1471e8
welington Ribeiro <wgt_tere@hotmail.com>
2023-05-24 08:30:39 -0300

    Novos teste de validação

---
+++ cypress/e2e/teste.cy.js
@@ -0,0 +1,6 @@
+describe('teste', () => {
+    it('teste', () => {
+        
+        cy.visit('https://passaporte2-hml.alterdata.com.br/?continue=https:%2F%2Ferpforme-hml.alterdata.com.br%2Fauth.html&nomeProduto=Koopon&idProduto=690bbae547bc55fbe4d2aaea8f9b733a')
+    });
+});
